df6eeb9c7973cddee987f5302866f6288425d16e
praneethkumarpidugu <pidugu2007@gmail.com>
2016-06-19 19:32:52 -0400

    form validation using angular-js

--- lesson5/main.js
+++ lesson5/main.js
@@ -4,15 +4,20 @@ var app = angular.module('minmax', []);
 app.controller('MinMaxCtrl', function ($scope, $http) {
 	$scope.formModel = {};
 
-	$scope.onSubmit = function () {
-		console.log("Hey i'm submitted!");
-		console.log($scope.formModel);
+	$scope.onSubmit = function (valid) {
+		if (valid) {
+			console.log("Hey i'm submitted!");
+			console.log($scope.formModel);
 
-		//$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
-		//	success(function (data) {
-		//		console.log(":)")
-		//	}).error(function(data) {
-		//		console.log(":(")
-		//	});
+			//$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+			//	success(function (data) {
+			//		console.log(":)")
+			//	}).error(function(data) {
+			//		console.log(":(")
+			//	});
+		} else {
+			console.log("Invalid Form!")
+		}
+
 	};
 });
